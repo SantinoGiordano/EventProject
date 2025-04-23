e37d9b730d8f66da9d7696e0c263d74bcbb17699
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-23 10:50:28 -0400

    css nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,11 @@ import Nav from "../components/Nav";
 export default function Home() {
   return (
     <>
+    <div className="bg-orange-50">
       <Nav />
+      <div className="pt-18"/>
       <div >Hello</div>
+    </div>
     </>
   );
 }
