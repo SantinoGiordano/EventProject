dbbd773a766756fc04c7f7f3be51ccc822a40933
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-23 15:05:44 -0400

    removed nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import Nav from "../components/Nav";
+
 
 export default function Home() {
   return (
     <>
     <div className="bg-orange-50">
-      <Nav />
       <div className="pt-18"/>
       <div >Hello</div>
     </div>
