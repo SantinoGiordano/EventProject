f7732eb1e4c980533bb920de56d295053084f1c2
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-25 16:31:49 -0400

    added more to config for monterey --font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
       extend: {
         fontFamily: {
@@ -6,5 +12,6 @@ module.exports = {
         },
       },
     },
+    plugins: [],
   };
   
