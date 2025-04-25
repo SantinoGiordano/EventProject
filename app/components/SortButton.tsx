66f02b42c4d37d37b68aaf9a2607397643528510
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-25 17:32:13 -0400

    created a very simple sort button that will have many functions

---
+++ app/components/SortButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SortButton = () => {
+  return (
+      <button className="flex items-center gap-2 px-4 py-2 bg-orange-100 text-orange-700 border border-orange-300 rounded-full hover:bg-orange-200 transition duration-200 shadow-sm">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M3 4a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h10a1 1 0 110 2H4a1 1 0 01-1-1zM3 16a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z"
+          />
+        </svg>
+        Sort Items
+      </button>
+  );
+};
+
+export default SortButton;
