60567829f059715eb094f0f0796d5be42f84abfd
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-28 12:54:33 -0400

    changed name from menu to path

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Drawer from "./Drawer";
+import path from "path";
 
 const Nav = () => {
   return (
@@ -40,7 +41,7 @@ const Nav = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
-                <span className="ml-2 hidden sm:inline">Menu</span>
+                <span className="ml-2 hidden sm:inline">My Notes</span>
               </label>
             </div>
             <div className="drawer-side z-[60]">
