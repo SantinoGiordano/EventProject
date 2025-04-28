f49a2a7f7dc37928f45a82b4cc92b549834407bb
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-28 09:35:53 -0400

    changed misspelling of typeEvents

--- utils/env.tsx
+++ utils/env.tsx
@@ -1,3 +1,3 @@
 export const APIEVENT_URI = 'http://localhost:8080'
 
-export const typeEvent = ['Business','Freinds','Personal','other']
+export const typeEvent = ['Business','Friends','Personal','other']
