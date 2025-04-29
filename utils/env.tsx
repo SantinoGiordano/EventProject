fa2271ce22c88229e660463566986637db86e3fc
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-29 10:23:08 -0400

    no slash to see if this changes route

--- utils/env.tsx
+++ utils/env.tsx
@@ -1,3 +1,3 @@
-export const APIEVENT_URI = 'https://eventproject-backend.onrender.com/'
+export const APIEVENT_URI = 'https://eventproject-backend.onrender.com'
 
 export const typeEvent = ['Business','Friends','Personal','other']
